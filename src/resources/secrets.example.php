2e1970171c47158134c273537b1b7a79ed4a6751
DiegoFleitas <diego.fleitas68@gmail.com>
2020-11-01 18:15:27 -0300

    secrets example

---
+++ src/resources/secrets.example.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego
+ * Date: 1/13/2019
+ * Time: 11:41 PM
+ */
+
+namespace ContentAwareBot\src\Bot\Classes;
+
+$_GIPHY_API_KEY = '';
+$_FFMPEG_PATH = '...\ffmpeg.exe';
+$_FFPROBE_PATH = '...\ffprobe.exe';
+
+//AWAREBOT
+$_APP_ID = '';
+$_APP_SECRET = '';
+$_CLIENT_TOKEN = '';
+$_PAGE_ID = '';
+//got it from https://maxman013.github.io/token/?fbclid=IwAR1KXWrvLwBf6eaflyvpkyzsNfCDNU8PbDvLMekSsxG5p-1OXBvjJOPxTn4
+$_ACCESS_TOKEN_DEBUG = '';
